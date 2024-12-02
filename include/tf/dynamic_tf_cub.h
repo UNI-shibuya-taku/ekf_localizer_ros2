5b5c4308e670f905a9bdb1bba1729bbfdf35d939
UNI-shibuya-taku <s.takuuuuu@gmail.com>
2024-12-02 23:59:56 +0900

    edit

--- include/tf/dynamic_tf_cub.h
+++ include/tf/dynamic_tf_cub.h
@@ -22,26 +22,14 @@ private:
     void pub_dynamic_tf();
     void pub_static_tf();
     void current_pose_callback(const geometry_msgs::msg::PoseStamped::ConstSharedPtr&);
-	// void ekf_pose_callback(const geometry_msgs::msg::PoseStamped::ConstSharedPtr& msg);
-
-    //member
-    // ros::NodeHandle nh;
-    // ros::NodeHandle private_nh;
-    // ros::Publisher pub_next_waypoint;
-    // ros::Subscriber sub_current_pose;
 
     rclcpp::Subscription<geometry_msgs::msg::PoseStamped>::SharedPtr sub_current_pose;
-    // rclcpp::Publisher<geometry_msgs::msg::PoseStamped>::SharedPtr ndt_pose_pub_;
 
     geometry_msgs::msg::PoseStamped current_pose;
-    // tf2_ros::TransformBroadcaster dynamic_br_;
-    // tf2_ros::TransformBroadcaster static_br_;
     std::shared_ptr<tf2_ros::TransformBroadcaster> dynamic_br_;
     std::shared_ptr<tf2_ros::TransformBroadcaster> static_br_;
 	std::unique_ptr<tf2_ros::Buffer> tfBuffer_;
 
-	// std::unique_ptr<tf2_ros::Buffer> tfBuffer_;
-    // std::shared_ptr<tf2_ros::TransformListener> tfListener_;
 };
 
 #endif
